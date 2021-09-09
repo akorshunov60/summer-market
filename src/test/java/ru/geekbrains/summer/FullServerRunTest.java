1a19db9517b741fcf460e211ec04a3e436b8f487
Alexander Korshunov <71963376+akorshunov60@users.noreply.github.com>
2021-09-09 15:25:31 +0300

    homeEx-7

--- src/test/java/ru/geekbrains/summer/FullServerRunTest.java
+++ src/test/java/ru/geekbrains/summer/FullServerRunTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.data.domain.Page;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -24,8 +25,8 @@ public class FullServerRunTest {
 //        headers.setContentType(MediaType.APPLICATION_JSON);
 //        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
 
-        List<?> categories = restTemplate.getForObject("/api/v1/categories", List.class);
-        assertThat(categories).isNotNull();
-        assertThat(categories).isNotEmpty();
+        Page<?> products = (Page<?>) restTemplate.getForObject("/api/v1/products", List.class);
+        assertThat(products).isNotNull();
+        assertThat(products).isNotEmpty();
     }
 }
