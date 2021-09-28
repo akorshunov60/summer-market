99b52d1ab32a836cf3f0edab8fe8b8bf99705c11
Alexander Korshunov <71963376+akorshunov60@users.noreply.github.com>
2021-09-28 13:47:36 +0300

    homeEx-10 настройки profiles.active

--- src/main/java/ru/geekbrains/summer/SummerMarketApplication.java
+++ src/main/java/ru/geekbrains/summer/SummerMarketApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+// Параметр VM Options для выбора профиля: -Dspring.profiles.active=dev (для версии Intellij IDEA не Ultimate)
+
 @PropertySource("classpath:security.properties")
 @SpringBootApplication
 public class SummerMarketApplication {
